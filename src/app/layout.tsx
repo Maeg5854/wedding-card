649967feb0a49de1083d0722d5ba6feca6af6701
Maeg5854 <kimhj585407@gmail.com>
2025-05-24 00:43:23 +0900

    ♻️ refactor non-used code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          src="//dapi.kakao.com/v2/maps/sdk.js?appkey=833aafa936ca686ac93a2d9136551c83"
-          strategy="beforeInteractive"
-        />
-      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
