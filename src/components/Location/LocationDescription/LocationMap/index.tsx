c24c9d852438a288e18f39c9c9641f753e51a377
Maeg5854 <kimhj585407@gmail.com>
2025-05-25 11:04:06 +0900

    ✨ add Map marker and adjust style

--- src/components/Location/LocationDescription/LocationMap/index.tsx
+++ src/components/Location/LocationDescription/LocationMap/index.tsx
@@ -1,13 +1,37 @@
 "use client";
 
-import { Map } from "react-kakao-maps-sdk";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 export default function LocationMap() {
+  const position = { lat: 37.5427105, lng: 126.9516296 };
   return (
     <Map
-      center={{ lat: 37.566826, lng: 126.9786567 }}
-      style={{ width: "500px", height: "300px", border: "1px solid black" }}
+      center={position}
+      style={{
+        width: "100%",
+        height: "300px",
+        boxShadow: "0 0 5px 0 rgba(0, 0, 0, 0.5)",
+      }}
       level={3}
-    />
+    >
+      <MapMarker position={position}>
+        <div
+          style={{
+            color: "#000000",
+            fontFamily: "SeoulNamsanCB",
+            padding: "10px",
+            paddingLeft: "35px",
+            borderRadius: "10px",
+            width: "100%",
+            fontSize: "14px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          아펠가모 공덕
+        </div>
+      </MapMarker>
+    </Map>
   );
 }
