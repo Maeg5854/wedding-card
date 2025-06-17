4336dd7698a59dc7eb68f7c440afa7e531cb24cd
Maeg5854 <kimhj585407@gmail.com>
2025-06-17 13:24:09 +0900

    ✨ 커버 데코레이션 추가

--- src/components/Conver/ConverPhotoFrame.tsx
+++ src/components/Conver/ConverPhotoFrame.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import Image from "next/image";
+import { PiHeartLight } from "react-icons/pi";
+
 export default function CoverPhotoFrame({
   style,
   src,
@@ -12,6 +15,8 @@ export default function CoverPhotoFrame({
       style={{
         display: "flex",
         justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
         ...style,
       }}
     >
@@ -30,6 +35,61 @@ export default function CoverPhotoFrame({
           <image href={src} width="300" height="450" />
         </g>
       </svg>
+      <div
+        style={{
+          zIndex: 1,
+          width: "300px",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          position: "absolute",
+          left: "50%",
+          transform: "translateX(-50%)",
+          padding: "0 10px",
+          gap: 2,
+          alignItems: "center",
+          bottom: 10,
+        }}
+      >
+        <Image
+          src={"/images/metamong_white.png"}
+          width={25}
+          height={22}
+          alt="damgomi"
+        />
+        <div>
+          <div>
+            <PiHeartLight
+              style={{
+                zIndex: 1,
+                position: "absolute",
+                left: "50%",
+                transform: "translateX(-50%)",
+                bottom: 10,
+              }}
+              size={20}
+              color="white"
+            />
+          </div>
+          <div
+            style={{
+              width: "calc(100% - 80px)",
+              height: "1px",
+              backgroundColor: "white",
+              position: "absolute",
+              bottom: 12,
+              left: "50%",
+              transform: "translateX(-50%)",
+            }}
+          />
+        </div>
+        <Image
+          src={"/images/damgomi_white.png"}
+          width={27.06}
+          height={23}
+          alt="damgomi"
+        />
+      </div>
     </div>
   );
 }
