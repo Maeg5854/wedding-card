0440d3826c81ffc74088929e9431660a4a5c2830
Maeg5854 <kimhj585407@gmail.com>
2025-06-22 23:02:30 +0900

    💄 Cover 내 불필요한 여백 삭제

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -21,7 +21,6 @@ export default function Cover() {
     <div
       style={{
         width: "100%",
-        height: "812px",
         display: "flex",
         flexDirection: "column",
         gap: "50px",
